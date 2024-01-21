cef7eb168741358ddaf3e1f6091dde674fe05476
Tadem Rechal shiny <95034942+shine2024@users.noreply.github.com>
2024-01-21 23:21:32 +0530

    35ms and 31ms solutions I found want to find more better and worst solutions than this

---
+++ 21-01-24#day2/1Length of Last Word.py
@@ -0,0 +1,25 @@
+# Runtime
+# 35
+# ms
+# Beats
+# 69.87%
+# of users with Python3
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        st=s.split()
+        c=st[-1]
+        
+        return len(c)
+    
+
+
+
+
+
+
+
+    
+
+
